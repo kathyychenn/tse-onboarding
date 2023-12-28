776b5da30ab193d9e8ef41e55c711211aec6a14a
kathyychenn <97640717+kathyychenn@users.noreply.github.com>
2023-12-27 22:25:28 -0700

    p1: TaskItem component

---
+++ frontend/src/components/TaskItem.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import type { Task } from "src/api/tasks";
+import { CheckButton } from "src/components";
+import styles from "src/components/TaskItem.module.css";
+
+export interface TaskItemProps {
+  task: Task;
+}
+
+export function TaskItem({ task }: TaskItemProps) {
+  let containerClass = styles.textContainer;
+  if (task.isChecked) {
+    containerClass += " " + styles.checked;
+  }
+  return (
+    <div className={styles.item}>
+      {/* render CheckButton here */}
+      <CheckButton checked={task.isChecked} />
+      <div className={containerClass}>
+        <span className={styles.title}>{task.title}</span>
+        {task.description && <span className={styles.description}>{task.description}</span>}
+      </div>
+    </div>
+  );
+}
