4acf98927852c45d29b6d948bf1a8a00b49ac83e
kathyychenn <97640717+kathyychenn@users.noreply.github.com>
2024-01-05 14:57:32 -0700

    2-2: Update to component: TaskItem

--- frontend/src/components/TaskItem.tsx
+++ frontend/src/components/TaskItem.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { updateTask, type Task } from "src/api/tasks";
 import { CheckButton } from "src/components";
 import styles from "src/components/TaskItem.module.css";
@@ -32,7 +33,9 @@ export function TaskItem({ task: initialTask }: TaskItemProps) {
     <div className={styles.item}>
       <CheckButton checked={task.isChecked} onPress={handleToggleCheck} disabled={isLoading} />
       <div className={containerClass}>
-        <span className={styles.title}>{task.title}</span>
+        <span className={styles.title}>
+          <Link to={"/task/" + task._id}>{task.title}</Link>
+        </span>
         {task.description && <span className={styles.description}>{task.description}</span>}
       </div>
     </div>
